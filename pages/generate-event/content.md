827cf37959ce2f037091e9e7edd0215a8d911ffa
Nathan Seva <thykof@protonmail.ch>
2023-02-01 15:04:13 +0100

    add pages and metadata

---
+++ pages/generate-event/content.md
@@ -0,0 +1,3 @@
+# Generate Event
+
+To generate an event on the blockchain from a smart contract, you can use the function `generateEvent` of the `massa-as-sdk` library.
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ import * as child from 'child_process';
 import * as fs from 'fs';
 import * as dotenv from 'dotenv';
 
+interface IMetaData {
+  dependencies: string[];
+}
+
+interface IRunRequest {
+  source: string;
+  metadata: IMetaData;
+}
+
 dotenv.config();
 
 const app = express();
@@ -13,14 +22,17 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use('/pages', express.static('pages'));
+
 app.get('/', (_, res) => res.send('🏠'));
 
 app.all('/run', run);
 
 app.listen(port, () => console.log('Running...'));
 
-function run(req: Request, res: Response) {
+function run(req: Request<object, object, IRunRequest>, res: Response) {
   const source = req.body.source;
+  const dependencies = req.body.metadata.dependencies;
   const timestamp = Date.now();
   // const projectName = `projects/project-${timestamp}`;
   const projectName = 'projects/project-1675246741617';
@@ -32,6 +44,10 @@ function run(req: Request, res: Response) {
       `npx @massalabs/sc-project-initializer@dev init ${projectName}`
     );
   }
+  child.execSync(`cd ${projectName}`);
+  dependencies.forEach((packageToInstall) => {
+    child.execSync(`cd ${projectName} && npm install ${packageToInstall}`);
+  });
   const minified =  source.replace(/^\s+|\s+$/g, '');
   fs.writeFileSync(`${projectName}/assembly/contracts/main.ts`, minified);
   fs.writeFileSync(`${projectName}/.env`, `
